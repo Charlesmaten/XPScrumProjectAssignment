e8c0734d714c75e23038e80a81aec48e629904be
Phillip Smith <github@psmith.dk>
2015-03-12 10:13:51 +0100

    Youssef, Phillip - Added student class with fields: Name, firstprio, secondprio

---
+++ src/main/java/Model/Student.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Phill
+ */
+public class Student {
+    public String name;
+    public Subject firstprio,secondprio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Subject getFirstprio() {
+        return firstprio;
+    }
+
+    public void setFirstprio(Subject firstprio) {
+        this.firstprio = firstprio;
+    }
+
+    public Subject getSecondprio() {
+        return secondprio;
+    }
+
+    public void setSecondprio(Subject secondprio) {
+        this.secondprio = secondprio;
+    }
+    
+    
+}
